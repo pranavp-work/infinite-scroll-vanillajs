511f73ce67690af4ca4391866b46117f420c180a
pranavp-work <pranavp.work@gmail.com>
2024-02-09 01:43:07 +0530

    Completed Project.

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 // Unsplash API
-const count = 10;
+const count = 30;
 const apiKey = 'crSjGO0gX6hgB8ZZO0LNQTVnqVRKI-NT-VTpuVNKp1M';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Checking if all images were finished loading/loaded.
+function imageLoaded() {
+    console.log("image has been loaded");
+    imagesLoaded++;
+    if (imagesLoaded === totalImages) {
+        ready = true;
+        loader.hidden = true;
+        console.log('ready =', ready);
+    }
+}
 
 // Helper function to set the attributes to DOM
 function setAttributes(element, attributes) {
@@ -17,6 +31,10 @@ function setAttributes(element, attributes) {
 
 // Create elements for links & photos, add to DOM.
 function displayPhotos() {
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
+    console.log("total images :", totalImages);
+
     photosArray.forEach((photo) => {
         const item = document.createElement('a');
         setAttributes(item, {
@@ -30,6 +48,9 @@ function displayPhotos() {
             alt: photo.alt_description,
             title: photo.alt_description,
         });
+
+        // Event listener for check if the images have finished loading...
+        img.addEventListener('load', imageLoaded);
 
         // Now, need to put <img> inside <a>, then put both inside imageContainer element.
         item.appendChild(img);
@@ -52,7 +73,8 @@ async function getPhotos() {
 
 // Check to see if scroll reaches bottom, then load more using GET
 window.addEventListener('scroll', ()=>{
-    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000) {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+        ready = false;
         getPhotos();
         console.log("Loading more and more");
     }
